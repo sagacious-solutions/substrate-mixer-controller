56cb8126867d2ba4743713cb2012c6b0468742ac
Matt Macdonald <mksmacdonald@gmail.com>
2024-11-02 18:55:05 -0700

    Complete initial clear, write and update code

--- modules/Display.py
+++ modules/Display.py
@@ -10,17 +10,35 @@ class Display :
   def __init__(self, display_config):
     self.config = display_config
     self.config["address"] = int(display_config.get("address", 0x27), 16)
-    self.lcdText = ["LCD Initialized..."]
+    self.lcdText = [
+      "LCD Initialized...",
+      "Line sdfklg",
+      "Some potato Text",
+      "Do Different stuff!!!"      
+      ]
         
     self.lcd = CharLCD(**self.config)
 
-    self.clear_line(0)
-    self.write_line(0, self.lcdText[0])
+    self.update_display()
+    print("LCD Initlialization Complete.")
+
+  def update_display(self):
+    for i in range(self.config.get("rows")) :
+      self.clear_line(i)
+
+      if i >= len(self.lcdText) :
+        break
+
+      self.write_line(i, self.lcdText[i])
 
   def write_line(self, line_num, str):
+    max_chars_on_line = self.config.get("cols")
+
+    if len(str) > max_chars_on_line :
+      raise ValueError(f"Each row can have no more than {max_chars_on_line} characters.")
+
     self.lcd.cursor_pos = (line_num, 0)
-    self.lcd.write_string(str)
-  
+    self.lcd.write_string(str)  
 
   def clear_line(self, line_num) :
     blank = ""
