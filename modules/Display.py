6441de3680986e98a3a159780def41576a22ef25
Matt Macdonald <mksmacdonald@gmail.com>
2024-11-02 18:32:25 -0700

    Initial build out of class for controlling LCD display

--- modules/Display.py
+++ modules/Display.py
@@ -2,12 +2,31 @@
 ## This module is for controlling output to the LCD for shaker
 ## Docs : https://rplcd.readthedocs.io/en/stable/getting_started.html#initializing-the-lcd
 ########################################################################################
-
+from multiprocessing import Process
 from RPLCD.i2c import CharLCD
+
 
 class Display :
   def __init__(self, display_config):
-    display_config["address"] = int(display_config.get("address", 0x27), 16)
-    self.lcd = CharLCD(**display_config)
+    self.config = display_config
+    self.config["address"] = int(display_config.get("address", 0x27), 16)
+    self.lcdText = ["LCD Initialized..."]
+        
+    self.lcd = CharLCD(**self.config)
 
-    self.lcd.write_string('New Words world')
+    self.clear_line(0)
+    self.write_line(0, self.lcdText[0])
+
+  def write_line(self, line_num, str):
+    self.lcd.cursor_pos = (line_num, 0)
+    self.lcd.write_string(str)
+  
+
+  def clear_line(self, line_num) :
+    blank = ""
+
+    for pos in range(self.config.get("cols")) :
+      blank += " "
+
+    self.lcd.cursor_pos = (line_num, 0)
+    self.lcd.write_string(blank)
